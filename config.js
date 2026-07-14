31a93d1be5efe7e8b6166fc9d54068ff226a33d6
varenyamwebsite-cpu <varenyamwebsite@gmail.com>
2026-07-14 16:51:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_PUBLIC_API_BASE_URL || "https://express-backend-x3lx.onrender.com/api";
+export const API_URL = import.meta.env.VITE_PUBLIC_API_BASE_URL || "https://express-backend-x3lx.onrender.com";
 export const STRAPI_URL = import.meta.env.VITE_STRAPI_URL || "https://strapi-cms-yw5e.onrender.com/api";
 
 export const client = axios.create({
